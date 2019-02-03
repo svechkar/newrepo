4b0fd5adc45d39e4f7d900d0a224871558ca11b6
svechkar <42885583+svechkar@users.noreply.github.com>
2019-02-03 14:45:38 +0200

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -1,35 +1,21 @@
 package com.company;
 
-import java.sql.SQLOutput;
-
 public class Calculator {
-    public addition(int a, int b) {
-        return a+b;
-        //System.out.println("Сложение двух чисел " + sum);
+    public int getSum(int a, int b) {
+        return  a + b;
     }
 
-    public differenceOff(int c, int d) {
-        return c-d;
-        //System.out.println("Разница двух чисел " + dif);
+    public int getDif(int a, int b) {
+        return  a - b;
     }
 
-    public multiplication(int e, int f) {
-        return e*f;
-        //System.out.println("Произведение двух чисел " + mult);
+    public int getMul(int a, int b) {
+        return  a * b;
     }
 
-    public division(int g, int h) {
-        return g/h;
-        //System.out.println("Частное двух чисел " + div);
+    public int getDiv(int a, int b) {
+        return  a / b;
     }
 
-    public static void remaindeOfTheDivision() {
-        int numb=9;
-        int rem = numb % 3;
-        if (rem == 0) {
-            System.out.println("Число делится на 3");
-        } else {
-            System.out.println("Число не делится на 3");
-        }
-    }
 }
+
--- Main.java
+++ Main.java
@@ -1,8 +1,29 @@
-package com.company;
+import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+       Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        if (sc.hasNextInt()) {
+            int a = sc.nextInt();
+
+          // System.out.println("You have entered " + number);
+        } else {
+            System.out.println("It is not a number! Please, enter a number.");
+        }
+
+        System.out.println("Enter second number:");
+        if (sc.hasNextInt()) {
+            int b = sc.nextInt();
+
+          //  System.out.println("You have entered " + number);
+        } else {
+            System.out.println("It is not a number! Please, enter a number.");
+        }
+
+        sc.close();
     }
 }
